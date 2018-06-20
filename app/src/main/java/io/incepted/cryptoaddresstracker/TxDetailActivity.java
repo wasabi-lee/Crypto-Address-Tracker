166e6552deaa54034b05e76c818f834ec343899d
Wasabi <hyclairelee@gmail.com>
2018-06-20 16:20:44 -0400

    tx detail layout

---
+++ app/src/main/java/io/incepted/cryptoaddresstracker/TxDetailActivity.java
@@ -0,0 +1,13 @@
+package io.incepted.cryptoaddresstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TxDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tx_detail);
+    }
+}
